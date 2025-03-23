8eebdb403c06b8845037bc9f1cd3a6d47cf8e61a
CaroKa <carolin.kanja@glueckkanja.com>
2025-03-23 14:34:51 +0100

    data(div): update copyright year to 2025

--- nuxt/src/runtime/plugin.js
+++ nuxt/src/runtime/plugin.js
@@ -47,7 +47,7 @@ export default defineNuxtPlugin((_nuxtApp) => {
 
   if (process.client) {
     // TODO read this from package.json . could be an issue because of the moving and the relative location of the file
-    console.debug('Shared Components v.0.1.116');
+    console.debug('Shared Components v.0.1.117');
 
     import('jquery')
       .then((module) => {
