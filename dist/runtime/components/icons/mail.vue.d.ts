36734671ca9b5c8d1826d763b4c28a1b2f4f50b9
Michael Ehrich <michael.ehrich@krekeny.com>
2025-04-24 00:16:09 +0200

    chore(package): 0.2.8

--- dist/runtime/components/icons/mail.vue.d.ts
+++ dist/runtime/components/icons/mail.vue.d.ts
@@ -2,10 +2,17 @@ declare namespace _default {
     let tagName: string;
     namespace computed {
         function style(): string;
+        function pathStyle(): string;
+        function strokeWidthValue(): number;
     }
     namespace props {
         let settings: ObjectConstructor;
         let color: StringConstructor;
+        namespace strokeWidth {
+            export let type: NumberConstructor;
+            let _default: number;
+            export { _default as default };
+        }
     }
 }
 export default _default;
