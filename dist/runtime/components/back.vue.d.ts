986ee706c18f3e3f0aa4efc7a6d9f5cac7cfeaec
Michael Ehrich <michael.ehrich@krekeny.com>
2025-05-19 19:56:25 +0200

    feat(back): adds vue component for native back button

---
+++ dist/runtime/components/back.vue.d.ts
@@ -0,0 +1,11 @@
+declare namespace _default {
+    let name: string;
+    namespace props {
+        namespace classes {
+            export let type: StringConstructor;
+            let _default: string;
+            export { _default as default };
+        }
+    }
+}
+export default _default;
