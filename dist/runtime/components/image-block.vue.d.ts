6f8a5eca2a07b9734c7f5530c03145303038e436
Michael Ehrich <michael.ehrich@krekeny.com>
2025-06-17 12:37:32 +0200

    feat(awards): migrates component

---
+++ dist/runtime/components/image-block.vue.d.ts
@@ -0,0 +1,18 @@
+declare namespace _default {
+    let tagName: string;
+    namespace computed {
+        function classList(): string[];
+        function imageWithCloudinary(): {
+            img: any;
+            cloudinary: boolean;
+            alt: any;
+        };
+    }
+    namespace props {
+        namespace imageBlock {
+            let _default: null;
+            export { _default as default };
+        }
+    }
+}
+export default _default;
