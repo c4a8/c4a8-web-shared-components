7e7cee675d5308608cdb329f6b0db6b5da1283a3
Lisa Nagl <lisa.nagl@glueckkanja.com>
2025-08-13 14:16:13 +0200

    fix(product-blocks): headline condition

--- nuxt/components/product-blocks.vue
+++ nuxt/components/product-blocks.vue
@@ -22,7 +22,7 @@
               <figure :class="['product-blocks__img', { 'mx-auto': item.copy === '' }, 'mb-4']">
                 <v-img :img="item.image" :cloudinary="item.cloudinary" :alt="item.alt" v-if="item.image" />
               </figure>
-              <h3 v-if="item - headline">{{ item.headline }}</h3>
+              <h3 v-if="item.headline">{{ item.headline }}</h3>
               <p v-if="item.copy">{{ item.copy }}</p>
               <ul v-if="item.bulletList" class="dashed">
                 <li v-for="(bulletListItem, bulletListIndex) in item.bulletList" :key="bulletListIndex">
