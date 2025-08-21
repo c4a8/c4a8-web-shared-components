b63c5da9488ec93431c6ce2e54cca9c325b9a2b9
Lisa Nagl <lisa.nagl@glueckkanja.com>
2025-08-21 16:15:10 +0200

    feat(fab-button): reworked fab-button component

--- dist/runtime/components/fab-button.vue.d.ts
+++ dist/runtime/components/fab-button.vue.d.ts
@@ -1,4 +1,5 @@
 declare namespace _default {
+    let tagName: string;
     namespace props {
         namespace icon {
             export let type: StringConstructor;
@@ -11,39 +12,60 @@ declare namespace _default {
             let _default_1: null;
             export { _default_1 as default };
         }
-        namespace bgColor {
-            let type_2: StringConstructor;
+        namespace noSticky {
+            let type_2: BooleanConstructor;
             export { type_2 as type };
-            let _default_2: null;
+            let _default_2: boolean;
             export { _default_2 as default };
         }
-        namespace iconColor {
+        namespace bgColor {
             let type_3: StringConstructor;
             export { type_3 as type };
             let _default_3: null;
             export { _default_3 as default };
         }
+        namespace iconColor {
+            let type_4: StringConstructor;
+            export { type_4 as type };
+            let _default_4: null;
+            export { _default_4 as default };
+        }
+        namespace trigger {
+            let type_5: (StringConstructor | NumberConstructor)[];
+            export { type_5 as type };
+            let _default_5: null;
+            export { _default_5 as default };
+        }
+    }
+    namespace computed {
+        function classList(): (string | {
+            [x: number]: any;
+        })[];
+        function iconStyle(): {
+            '--color-fab-background': any;
+            color: any;
+        };
+        function offsetTop(): number;
     }
     function data(): {
-        modalDisplay: string;
-        buttonDisplay: string;
+        resetDelay: number;
+        isExpanded: boolean;
+        hasTrigger: boolean;
+        expandedClass: string;
+        offScreenClass: string;
+        hasTriggerClass: string;
     };
-    namespace computed {
-        function fabButtonClass(): string[];
-        function fabButtonIconStyle(): {
-            display: any;
-            color?: any;
-            '--color-fab-background'?: any;
-        };
-        function modalStyle(): any;
-    }
     function mounted(): void;
-    function beforeDestroy(): void;
     namespace methods {
+        function init(): void;
         function bindEvents(): void;
-        function unbindEvents(): void;
+        function bindTriggerEvent(): void;
+        function handleTriggerClick(e: any): void;
         function handleOutsideClick(e: any): void;
-        function toggleModal(): void;
+        function handleSubmit(): void;
+        function handleClose(): void;
+        function handleClick(): void;
     }
+    function beforeDestroy(): void;
 }
 export default _default;
--- nuxt/components/fab-button.vue
+++ nuxt/components/fab-button.vue
@@ -1,95 +1,163 @@
 <template>
-  <div :class="fabButtonClass" ref="root">
-    <div class='fab-button__wrapper'>
-      <div v-if="modal" ref="modal" :style="modalStyle" class="fab-button__modal sticky-bottom">
-        <div class="fab-button__close" ref="close" @click="toggleModal">
-          <icon icon="close" :circle="true" :hover="true" size="medium" />
-        </div>
-        <contact v-if="modal.contact" :contact="modal.contact.infos" :collapsed="true" />     
-        <span v-if="modal.content">{{ modal.content }}</span>
-        <slot></slot>
-      </div>
-      <div class="fab-button__icon sticky-bottom" :style="fabButtonIconStyle" ref="icon" @click="toggleModal">
-        <icon :icon="icon" size="large" />
-      </div>
+    <div :class="classList" ref="root">
+        <div class="fab-button__start"></div>
+        <sticky-block :sticky-offset-top="offsetTop">
+            <div class='fab-button__wrapper' :class="['fab-button__wrapper', !noSticky ? 'js-sticky-block' : '']">
+                <div v-if="modal" class="fab-button__modal is-off-screen" ref="modal">
+                    <div class="fab-button__close" ref="close">
+                        <icon icon="close" :circle="true" :hover="true" size="medium" />
+                    </div>
+                    <contact v-if="modal.contact" :contact="modal.contact.infos" :collapsed="true" :ajax="true" />
+                </div>
+                <div class="fab-button__icon" ref="icon" :style="iconStyle"
+                    v-bind="trigger ? { 'data-trigger-id': trigger } : {}">
+                    <icon :icon="icon" size="large" />
+                </div>
+            </div>
+            
+        </sticky-block>
+        <a class="fab-trigger" ref="link"></a>
     </div>
-  </div>
 </template>
 
 <script>
+import State from '../utils/state.js';
+import Events from '../utils/events.js';
 import Tools from '../utils/tools.js';
 
 export default {
-  props: {
-    icon: {
-      type: String,
-      default: 'phone-mail'
+    tagName: 'fab-button',
+    props: {
+        icon: {
+            type: String,
+            default: 'phone-mail'
+        },
+        modal: {
+            type: Object,
+            default: null
+        },
+        noSticky: {
+            type: Boolean,
+            default: false
+        },
+        bgColor: {
+            type: String,
+            default: null
+        },
+        iconColor: {
+            type: String,
+            default: null
+        },
+        trigger: {
+            type: [String, Number],
+            default: null
+        }
     },
-    modal: {
-      type: Object,
-      default: null
+    computed: {
+        classList() {
+            return ['fab-button',
+                !this.noSticky ? 'fab-button--sticky' : '',
+                this.trigger ? 'fab-button--has-trigger' : '',
+                { [this.expandedClass]: this.isExpanded },
+                { [this.hasTriggerClass]: this.hasTrigger }
+            ];
+        },
+        iconStyle() {
+            let style = {};
+            if (this.bgColor) style['--color-fab-background'] = this.bgColor;
+            if (this.iconColor) style.color = this.iconColor;
+            return style;
+        },
+        offsetTop() {
+            return window.innerHeight * 0.97;
+        }
     },
-    bgColor: {
-      type: String,
-      default: null
+    data() {
+        return {
+            resetDelay: 300,
+            isExpanded: false,
+            hasTrigger: false,
+            expandedClass: State.EXPANDED,
+            offScreenClass: State.OFF_SCREEN,
+            hasTriggerClass: 'fab-button--has-trigger',
+        };
     },
-    iconColor: {
-      type: String,
-      default: null
+    mounted() {
+        this.iconElement = this.$refs.icon;
+        this.linkElement = this.$refs.link instanceof NodeList ? this.$refs.link : [this.$refs.link];
+        this.modalElement = this.$refs.modal;
+        this.closeElement = this.$refs.close;
+        this.root = this.$refs.root;
+
+        this.hasTrigger = this.root.classList.contains(this.hasTriggerClass);
+
+        this.init();
     },
-  },
-  data() {
-    return {
-      modalDisplay: 'none',
-      buttonDisplay: 'block'
-    };
-  },
-  computed: {
-    fabButtonClass() {
-      return [
-        'fab-button',
-      ];
+    methods: {
+        init() {
+            this.bindEvents();
+        },
+        bindEvents() {
+            if (!this.iconElement || !this.modalElement || this.hasTrigger) return this.bindTriggerEvent();
+
+            this.linkElement.forEach((link) => {
+                link.addEventListener('click', this.handleClick);
+            });
+            this.iconElement.addEventListener('click', this.handleClick);
+            this.closeElement?.addEventListener('click', this.handleClose);
+
+            document.addEventListener(Events.FORM_AJAX_SUBMIT, this.handleSubmit);
+            window.addEventListener('click', this.handleOutsideClick);
+
+            this.modalElement.style.opacity = '1';
+            this.modalElement.style.opacity = '';
+        },
+        bindTriggerEvent() {
+            this.iconElement.addEventListener('click', this.handleTriggerClick);
+        },
+        handleTriggerClick(e) {
+            const target = e.currentTarget;
+            const triggerId = target.dataset.triggerId;
+
+            document.dispatchEvent(new CustomEvent(Events.OPEN_MODAL, { detail: { id: triggerId } }));
+        },
+        handleOutsideClick(e) {
+            if (
+                this.root.classList.contains(this.expandedClass) &&
+                Tools.isOutsideOf('fab-button', e) &&
+                Tools.isOutsideOf('fab-trigger', e)
+            ) {
+                this.handleClose();
+            }
+            if(!Tools.isOutsideOf('fab-trigger', e)) {
+                this.handleClick();
+            }
+        },
+        handleSubmit() {
+            this.handleClose();
+        },
+        handleClose() {
+            this.handleClick();
+
+            setTimeout(() => {
+                document.dispatchEvent(
+                    new CustomEvent(Events.FAB_BUTTON_CLOSE, {
+                        detail: { target: this.root },
+                    })
+                );
+            }, this.resetDelay);
+        },
+        handleClick() {
+            this.isExpanded = !this.isExpanded;
+            this.modalElement.classList.toggle(this.offScreenClass);
+            if (this.modalElement.classList.contains(this.offScreenClass)) {
+                this.modalElement.style.opacity = '';
+            }
+        },
     },
-    fabButtonIconStyle() {
-      return {
-        ...(this.bgColor ? { '--color-fab-background': this.bgColor } : {}),
-        ...(this.iconColor ? { color: this.iconColor } : {}),
-        display: this.buttonDisplay,
-      };
+    beforeDestroy() {
+        window.removeEventListener('click', this.handleOutsideClick);
+        document.removeEventListener(Events.FORM_AJAX_SUBMIT, this.handleSubmit);
     },
-    modalStyle() {
-      return {
-        display: this.modalDisplay
-      };
-    },
-  },
-  mounted() {
-    this.bindEvents();
-  },
-  beforeDestroy() {
-    this.unbindEvents();
-  },
-  methods: {
-    bindEvents() {
-      window.addEventListener('click', this.handleOutsideClick);
-    },
-    unbindEvents() {
-      window.removeEventListener('click', this.handleOutsideClick);
-    },
-    handleOutsideClick(e) {
-      if (
-        this.modalDisplay === 'block' &&
-        Tools.isOutsideOf('fab-button', e) &&
-        Tools.isOutsideOf('fab-trigger', e)
-      ) {
-        this.toggleModal();
-      }
-    },
-    toggleModal() {
-      const isOpen = this.modalDisplay === 'block';
-      this.modalDisplay = isOpen ? 'none' : 'block';
-      this.buttonDisplay = isOpen ? 'block' : 'none';
-    }
-  }
 };
 </script>
