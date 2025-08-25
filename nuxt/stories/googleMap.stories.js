d7992697a0fc92f734b69b240429f3426eb0343d
Lisa Nagl <lisa.nagl@glueckkanja.com>
2025-08-25 14:44:25 +0200

    feat(googple-map): image-slider option

--- nuxt/stories/googleMap.stories.js
+++ nuxt/stories/googleMap.stories.js
@@ -47,3 +47,46 @@ export const GoogleMap = {
     ],
   },
 };
+
+export const WithImageSlider = {
+  args: {
+    location: {
+      street: 'Kaiserstraße 39',
+      city: '63065 Offenbach am Main',
+      office: 'Hauptsitz',
+      coordinates: [50.10343087097074, 8.760059035466721],
+    },
+    entries: [
+      {
+        icon: 'phone',
+        headline: 'Rufen Sie uns an',
+        subline: '+49 123456789',
+      },
+      {
+        icon: 'email-action-unread',
+        headline: 'Schreiben Sie uns',
+        subline: 'loremipsum@yahoo.com',
+      },
+      {
+        icon: 'pin',
+        headline: 'Kommen Sie Vorbei',
+        subline: 'Kaiserstraße 39 <br /> 63065 Offenbach am Main',
+      },
+      {
+        icon: 'phone-charger',
+        headline: 'Mit einem Elektrofahrzeug zu uns',
+        subline: 'E-Tankstellen in unserer Tiefgarage',
+      },
+    ],
+        images: [
+      {
+        img: "/company/locations/Offenbach/_JR_8285-HDR.jpg",
+        cloudinary: true
+      },
+      {
+        img: "/company/locations/Offenbach/gk-Sinclair.jpg",
+        cloudinary: true
+      }
+    ]
+  },
+};
