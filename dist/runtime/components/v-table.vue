ec6d5c8486f095ef2f0999ed1722c08123a3d48e
Lisa Nagl <lisa.nagl@glueckkanja.com>
2025-08-26 15:02:07 +0200

    feat(v-table): agenda variant, table stories

--- dist/runtime/components/v-table.vue
+++ dist/runtime/components/v-table.vue
@@ -1,23 +1,38 @@
 <template>
-  <div :class="[tableHideContainer ? 'mb-8' : 'container', 'mt-8 mt-lg-10']">
-    <div :class="[!tableHideContainer ? 'row' : '']">
-      <div :class="['table-responsive', classes]">
-        <table :class="['v-table table', styleClass]">
-          <thead v-if="head && table.length">
-            <tr>
-              <th v-for="(col, colIndex) in table[0]" :key="'head-' + colIndex">
-                {{ col }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(row, rowIndex) in tableRows" :key="'row-' + rowIndex">
-              <td v-for="(col, colIndex) in row" :key="'cell-' + rowIndex + '-' + colIndex">
-                {{ col }}
+  <div>
+    <div :class="[tableHideContainer ? 'mb-8' : 'container', 'mt-8 mt-lg-10']">
+      <div :class="[!tableHideContainer ? 'row' : '']">
+        <div :class="['table-responsive', classes]">
+          <headline level="h2" class="space-bottom-1">{{ headline }}</headline>
+          <table v-if="agenda" class="agenda">
+            <tr v-for="item in table" :key="item.id">
+              <td>
+               <span>{{ item.timeFrom }}</span><span v-if="item.timeTo"> - {{ item.timeTo }}</span>
+                <br>
+                <strong>{{ item.title }}</strong>
+                <br>
+                <em v-if="item.speaker" :style="item.highlight ? '--color-highlight-underline: var(--color-yellow)' : '--color-highlight-underline: null'" class="highlight-underline">{{ item.speaker }}</em>
+                <p>{{ item.description }}</p>
               </td>
             </tr>
-          </tbody>
-        </table>
+          </table>
+          <table v-else :class="['v-table table', styleClass]">
+            <thead v-if="head && table.length">
+              <tr>
+                <th v-for="(col, colIndex) in table[0]" :key="'head-' + colIndex">
+                  {{ col }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(row, rowIndex) in tableRows" :key="'row-' + rowIndex">
+                <td v-for="(col, colIndex) in row" :key="'cell-' + rowIndex + '-' + colIndex">
+                  {{ col }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -26,6 +41,10 @@
 export default {
   tagName: 'v-table',
   props: {
+    headline: {
+      type: String,
+      default: null,
+    },
     table: {
       type: Array,
       required: true,
@@ -46,6 +65,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    agenda: {
+      type: Boolean,
+      default: false,
+    },
   },
   computed: {
     tableHideContainer() {
@@ -57,6 +80,7 @@ export default {
     tableRows() {
       return this.head ? this.table.slice(1) : this.table;
     },
-  },
+
+  }
 };
 </script>
