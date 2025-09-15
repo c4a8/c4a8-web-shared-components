d8e522d1c2222b18b53f11b8601365b97432080a
Lisa Nagl <lisa.nagl@glueckkanja.com>
2025-09-15 13:58:12 +0200

    feat(newsletter)

---
+++ nuxt/components/newsletter.vue
@@ -0,0 +1,29 @@
+<template >
+    <div class="newsletter" style="margin-top: 1000px" >
+
+
+            <div class="newsletter__wrapper" >
+                <div class="newsletter__modal" >
+                    <div class="newsletter__close" ref="close">
+                        <icon icon="close" :circle="true" :hover="true" size="medium" />
+                    </div>
+                    <headline level="h2">Newsletter Subscription</headline>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
+    
+            </div>
+            
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    tagName: 'newsletter',
+    props: {
+    }
+ 
+
+}
+</script>
