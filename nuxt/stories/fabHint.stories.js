3ce574e10deded2d98b5b2d6371a0280b0f60f5a
Michael Ehrich <michael.ehrich@krekeny.com>
2025-10-22 20:41:33 +0200

    feat(fab-hint): adds basic NOT working migration

--- nuxt/stories/fabHint.stories.js
+++ nuxt/stories/fabHint.stories.js
@@ -1,9 +1,19 @@
-import FabHint from "../../components/fab-hint";
+import FabHint from '../components/fab-hint.vue';
 
 export default {
   component: FabHint,
   argTypes: {},
-  title: "Components/Fab Hint",
+  title: 'Components/Fab Hint',
+  decorators: [
+    (story) => ({
+      components: { story },
+      template: `
+        <div style="height: 50vh;" class="dummy-content"></div>
+        <story />
+        <div style="height: 500vh;" class="dummy-content"></div>
+      `,
+    }),
+  ],
 };
 
 export const Default = {
@@ -14,10 +24,10 @@ export const Default = {
 
 export const BackgroundColor = {
   args: {
-    bgColor: "var(--color-orange)",
-    iconColor: "var(--color-white)",
-    icon: "world",
-    title: "Lorem ipsum dolor",
+    bgColor: 'var(--color-orange)',
+    iconColor: 'var(--color-white)',
+    icon: 'world',
+    title: 'Lorem ipsum dolor',
     text: 'Slider: © lorem; Kacheln: 1: © lorem; 2: © Author - example.com - 123456789; 3: © Author - example.com - 123456789; 4: © example.com; CTA Jobs: © Author - example.com - 123456789; Kacheln: 1: © <a href="https://www.example.com/">https://www.example.com/</a>; 2: © lorem; 3: © <a href="https://www.example.com/">https://www.example.com/</a>; CTA: © Author - example.com - 123456789',
   },
 };
