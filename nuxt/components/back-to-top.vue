65a7ace0cedd754842137b738ec352cc290b5f19
Michael Ehrich <michael.ehrich@krekeny.com>
2025-11-06 17:14:34 +0100

    feat(back-to-top): adds bare version of component

---
+++ nuxt/components/back-to-top.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="back-to-top">
+    <div class="back-to-top__icon">
+      <icon :icon="arrow" size="large" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  tagName: 'back-to-top',
+  // computed: {
+  // },
+};
+</script>
