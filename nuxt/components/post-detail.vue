367fcd79c5bd1d8d2f3c3718706689267b452a36
Sam Sauer <sam.sauer@krekeny.com>
2026-01-12 18:03:47 +0100

    refactor(post-detail): enhance layout with content grid and breakout classes for further grid placements

--- nuxt/components/post-detail.vue
+++ nuxt/components/post-detail.vue
@@ -1,95 +1,91 @@
 <template>
-  <div class="post-detail container space-top-2 space-top-lg-4" :class="{ 'post-detail--aside-nav': asideNavValue }">
-    <div class="w-xl-80 mx-xl-auto">
-      <article
-        v-if="normalizedPost"
-        class="post h-entry"
-        itemscope
-        itemtype="http://schema.org/BlogPosting"
-        ref="article"
-      >
-        <header class="post-header">
-          <h1
-            class="post-title p-name"
-            :class="normalizedPost.titleClass || 'h2-font-size'"
-            itemprop="name headline"
-            v-html="normalizedPost.title"
-          ></h1>
-          <div class="py-4 mt-5">
-            <div class="row align-items-md-center">
-              <div class="col-md-7 mb-5 mb-md-0" v-if="post.author">
-                <div class="media align-items-center">
-                  <div v-for="person in avatars" :key="person" class="avatar mr-2">
-                    <author-avatar
-                      v-if="authors && authors[person]?.avatar"
-                      :img-url="authors[person]?.avatar"
-                      :author="authors[person]"
-                    />
-                  </div>
-                  <div class="media-body font-size-1" :class="{ 'ml-3': avatars.length > 0 }">
-                    <authors :authors-list="normalizedPost.author" :data-authors="authors" />
-                  </div>
-                </div>
+  <article
+    v-if="normalizedPost"
+    class="post post-detail h-entry content-grid space-top-2 space-top-lg-4"
+    :class="{ 'post-detail--aside-nav': asideNavValue }"
+    itemscope
+    itemtype="http://schema.org/BlogPosting"
+    ref="article"
+  >
+    <header class="post-header">
+      <h1
+        class="post-title p-name"
+        :class="normalizedPost.titleClass || 'h2-font-size'"
+        itemprop="name headline"
+        v-html="normalizedPost.title"
+      ></h1>
+      <div class="py-4 mt-5">
+        <div class="row align-items-md-center">
+          <div class="col-md-7 mb-5 mb-md-0" v-if="post.author">
+            <div class="media align-items-center">
+              <div v-for="person in avatars" :key="person" class="avatar mr-2">
+                <author-avatar
+                  v-if="authors && authors[person]?.avatar"
+                  :img-url="authors[person]?.avatar"
+                  :author="authors[person]"
+                />
               </div>
-              <div class="col-md-5 align-self-end pr-lg-0" :class="{ 'pl-lg-0': !post.author }">
-                <div class="text-muted font-size-1" :class="{ 'text-right': post.author }">
-                  <time class="dt-published" :datetime="formattedDateXml" itemprop="datePublished">
-                    {{ formattedDate }}
-                  </time>
-                </div>
+              <div class="media-body font-size-1" :class="{ 'ml-3': avatars.length > 0 }">
+                <authors :authors-list="normalizedPost.author" :data-authors="authors" />
               </div>
             </div>
           </div>
-        </header>
-        <div class="post-content e-content" itemprop="articleBody">
-          <p class="post__sticky-excerpt" v-html="normalizedPost.customExcerpt || normalizedPost.excerpt"></p>
-          <div v-if="post.blogTitleImages" class="post__title-images">
-            <slider :hide-background="true">
-              <v-img
-                v-for="(postTitleImage, index) in normalizedPost.blogTitleImages"
-                :key="index"
-                :cloudinary="postTitleImage.cloudinary"
-                :img="postTitleImage.img"
-              />
-            </slider>
+          <div class="col-md-5 align-self-end pr-lg-0" :class="{ 'pl-lg-0': !post.author }">
+            <div class="text-muted font-size-1" :class="{ 'text-right': post.author }">
+              <time class="dt-published" :datetime="formattedDateXml" itemprop="datePublished">
+                {{ formattedDate }}
+              </time>
+            </div>
           </div>
-          <div v-else-if="normalizedPost.blogtitlepic" class="post__intro-img">
-            <v-img
-              :img="blogImagePath + normalizedPost.blogtitlepic"
-              :cloudinary="true"
-              :alt="normalizedPost.title"
-              img-src-sets="postImages"
-            />
-            <figcaption v-if="normalizedPost.blogtitlepicsubline">{{ normalizedPost.blogtitlepicsubline }}</figcaption>
-          </div>
-          <sticky-block
-            v-if="shouldShowStickyBlocks"
-            class="post__sticky-bar"
-            :sticky-offset-top="stickyOffsetTop"
-            :has-padding="!asideNavValue"
-            breakpoint="lg"
-            :calculate-height="true"
-          >
-            <aside-nav v-if="asideNavValue" v-bind="asideNavValue" />
-            <socials :vertical="true" :hide-label="true" :author="null" :share-url="shareUrl" v-else />
-          </sticky-block>
-
-          <shared-content-renderer
-            :value="enhancedPost"
-            tag="main"
-            :class="contentWidth"
-            :components="{ a: ContentRendererLink }"
-          />
-          <div class="post-detail__tags mt-5">
-            <tag v-for="(tag, index) in normalizedPost.tags" :key="index" :tag="tag" variant="small" />
-          </div>
-          <aside-nav v-if="!shouldShowStickyBlocks && asideNavValue" v-bind="asideNavValue" />
         </div>
-      </article>
-      <div v-else>
-        <h1>Post not found</h1>
       </div>
+    </header>
+    <p class="post__sticky-excerpt" v-html="normalizedPost.customExcerpt || normalizedPost.excerpt"></p>
+    <div v-if="post.blogTitleImages" class="post__title-images">
+      <slider :hide-background="true">
+        <v-img
+          v-for="(postTitleImage, index) in normalizedPost.blogTitleImages"
+          :key="index"
+          :cloudinary="postTitleImage.cloudinary"
+          :img="postTitleImage.img"
+        />
+      </slider>
     </div>
+    <div v-else-if="normalizedPost.blogtitlepic" class="post__intro-img breakout">
+      <v-img
+        :img="blogImagePath + normalizedPost.blogtitlepic"
+        :cloudinary="true"
+        :alt="normalizedPost.title"
+        img-src-sets="postImages"
+      />
+      <figcaption v-if="normalizedPost.blogtitlepicsubline">{{ normalizedPost.blogtitlepicsubline }}</figcaption>
+    </div>
+    <sticky-block
+      v-if="shouldShowStickyBlocks"
+      class="post__sticky-bar breakout"
+      :sticky-offset-top="stickyOffsetTop"
+      :has-padding="!asideNavValue"
+      breakpoint="lg"
+      :calculate-height="true"
+    >
+      <aside-nav v-if="asideNavValue" v-bind="asideNavValue" />
+      <socials :vertical="true" :hide-label="true" :author="null" :share-url="shareUrl" v-else />
+    </sticky-block>
+
+    <shared-content-renderer
+      :value="enhancedPost"
+      tag="main"
+      :class="contentWidth"
+      itemprop="articleBody"
+      :components="{ a: ContentRendererLink }"
+    />
+    <div class="post-detail__tags mt-5">
+      <tag v-for="(tag, index) in normalizedPost.tags" :key="index" :tag="tag" variant="small" />
+    </div>
+    <aside-nav v-if="!shouldShowStickyBlocks && asideNavValue" v-bind="asideNavValue" />
+  </article>
+  <div v-else>
+    <h1>Post not found</h1>
   </div>
 </template>
 
