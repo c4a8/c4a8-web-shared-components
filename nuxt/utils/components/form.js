3f976df6ee510407258d5f802903eb7358ae46ce
Lisa Nagl <lisa.nagl@glueckkanja.com>
2026-02-24 17:38:44 +0100

    feat(contact): add loader animation

--- nuxt/utils/components/form.js
+++ nuxt/utils/components/form.js
@@ -296,7 +296,7 @@ class Form extends BaseComponent {
   // }
 
   submitOdooForm() {
-    fetch('https://webinar-test2-function.azurewebsites.net/api/events/register', {
+    fetch('https://webinar-prod-function.azurewebsites.net/api/events/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
