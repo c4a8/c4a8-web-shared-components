1a56b0ce892dd6b7e5aa8a79284e796e9a56cdad
Michael Ehrich <michael.ehrich@krekeny.com>
2026-03-03 12:09:51 +0100

    feat(code-block): use slot and remove trimming

--- nuxt/components/code-block.vue
+++ nuxt/components/code-block.vue
@@ -3,22 +3,15 @@
     <button class="code-block__copy" :class="{ 'code-block__copy--copied': copied }" @click="copyCode">
       {{ copied ? 'Copied!' : 'Copy' }}
     </button>
-    <code ref="codeRef" class="code-block__code">{{ processedCode }}</code>
+    <code ref="codeRef" class="code-block__code"><slot /></code>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, useSlots } from 'vue';
+import { ref } from 'vue';
 
 const codeRef = ref(null);
 const copied = ref(false);
-const slots = useSlots();
-
-const processedCode = computed(() => {
-  const content = slots.default?.()[0]?.children;
-
-  return typeof content === 'string' ? content.trim() : content;
-});
 
 function copyCode() {
   const text = codeRef.value?.innerText ?? '';
