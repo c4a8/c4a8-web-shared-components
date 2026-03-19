54ee96034fbe638f525c2acd3c76dca4ffb8e964
Michael Ehrich <michael.ehrich@krekeny.com>
2026-03-19 17:05:05 +0100

    feat(card-box-animation): add wip state

---
+++ nuxt/components/card-box-animation.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card-box-animation">
+    <div class="card-box-animation__intro-wrapper">
+      <div class="card-box-animation__box">
+        <div class="card-box-animation__box-flap card-box-animation__box-flap--left"></div>
+        <div class="card-box-animation__box-flap card-box-animation__box-flap--right"></div>
+        <div class="card-box-animation__box-back">
+          <div class="card-box-animation__box-back-face"></div>
+        </div>
+        <div
+          class="card-box-animation__card card-box-animation__card--3"
+          style="--rot: 0deg; --tx: 0%; --ty: -160%"
+        ></div>
+        <div
+          class="card-box-animation__card card-box-animation__card--4"
+          style="--rot: -25deg; --tx: -20%; --ty: -160%"
+        ></div>
+        <div
+          class="card-box-animation__card card-box-animation__card--5"
+          style="--rot: -50deg; --tx: -40%; --ty: -155%"
+        ></div>
+        <div class="card-box-animation__box-front">
+          <div class="card-box-animation__box-front-face"></div>
+        </div>
+        <div class="card-box-animation__box-left"></div>
+        <div class="card-box-animation__box-right"></div>
+        <div class="card-box-animation__box-bottom"></div>
+        <div class="card-box-animation__box-lid">
+          <div class="card-box-animation__box-lid-inner">
+            <div class="card-box-animation__box-lid-top">
+              <div class="card-box-animation__box-lid-top-logo"></div>
+              <div class="card-box-animation__box-lid-top-back"></div>
+            </div>
+            <div class="card-box-animation__box-lid-flap">
+              <div class="card-box-animation__box-lid-flap-shape">
+                <div class="card-box-animation__box-lid-flap-shape-inner"></div>
+                <div class="card-box-animation__box-lid-flap-shape-outer"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ nuxt/stories/cardBoxAnimation.stories.js
@@ -0,0 +1,11 @@
+import CardBoxAnimation from '../components/card-box-animation.vue';
+
+export default {
+  title: 'Components/Card Box Animation',
+  component: CardBoxAnimation,
+  argTypes: {},
+};
+
+export const Default = {
+  args: {},
+};
