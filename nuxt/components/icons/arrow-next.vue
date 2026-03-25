25e4fdf9f48cad1d206461017e734312f5b89d6e
Lisa Nagl <lisa.nagl@glueckkanja.com>
2026-03-25 15:39:28 +0100

    feat(location-section)

--- nuxt/components/icons/arrow-next.vue
+++ nuxt/components/icons/arrow-next.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg v-bind="settings" width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle
       opacity="0.4"
       cx="26.8889"
@@ -21,21 +21,8 @@
 <script>
 export default {
   tagName: 'arrow-next',
-  computed: {
-    style() {
-      return `stroke: ${this.color}; transform: rotate(var(--icon-rotation)) scale(var(--icon-scale));`;
-    },
-    strokeWidthValue() {
-      return this.strokeWidth / 2.66666666;
-    },
-  },
   props: {
     settings: Object,
-    color: String,
-    strokeWidth: {
-      type: Number,
-      default: 4,
-    },
   },
 };
 </script>
