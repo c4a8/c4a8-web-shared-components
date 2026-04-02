7c8dc5e050db9a4e31a6fe13d60cd76c8a0887be
Lisa Nagl <lisa.nagl@glueckkanja.com>
2026-04-02 10:33:52 +0200

    data(src): add testimonials slug

--- nuxt/src/module.ts
+++ nuxt/src/module.ts
@@ -142,6 +142,14 @@ export default defineNuxtModule({
 
     extendPages((pages) => {
       pages.unshift({
+        name: 'slug-testimonials',
+        path: '/testimonials/:slug(.*)*',
+        file: resolve('./runtime/pages/testimonials/[...slug].vue'),
+      });
+    });
+
+    extendPages((pages) => {
+      pages.unshift({
         name: 'slug-all',
         path: '/:slug(.*)*',
         file: resolve('./runtime/pages/[...slug].vue'),
