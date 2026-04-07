e8179d23b7d4291bc8ccbd0b3cb4c7c5004244bb
Lisa Nagl <lisa.nagl@glueckkanja.com>
2026-04-07 16:08:48 +0200

    feat(testimonial-detail): simplify testimonial-detail

--- nuxt/components/testimonial-detail.vue
+++ nuxt/components/testimonial-detail.vue
@@ -1,40 +1,32 @@
 <template>
-  <div
-    :class="['testimonials-detail', 'page-detail', 'page-detail--image', light ? 'page-detail--light' : '']"
+  <page-detail
     :style="{
       '--page-detail-color': `var(${detailColor})`,
-      '--color-icon-hover-color': 'var(--page-detail-color)',
+      '--color-icon-hover-color': `var(${detailColor})`,
     }"
   >
-    <div class="page-detail__container container">
-      <div class="page-detail__start row">
-        <div class="page-detail__sticky-start col-md-11 col-lg-5">
-          <sticky-block class="page-detail__intro js-sticky-block" :data-hs-sticky-block-options="stickyOptions">
-            <headline
-              :text="computedName"
-              :level="headlineLevel"
-              :classes="
-                'testimonials-detail__headline page-detail__headline pt-5' +
-                (headlineClasses ? ` ${headlineClasses}` : ' h2-font-size bold')
-              "
-            />
-            <span class="testimonials-detail__subline page-detail__subline">{{ computedSubline }}</span>
-            <div class="page-detail__img page-detail__animation-3">
-              <v-img v-bind="image" :cloudinary="!cloudinary ? true : image.cloudinary" />
-            </div>
-          </sticky-block>
-        </div>
-        <div class="page-detail__content page-detail__animation-3 col-md-11 offset-lg-1 col-lg-6">
-          <div class="page-detail__description richtext has-no-border">
-            <p>{{ introText }}</p>
-            <ContentRenderer v-if="body" :value="body" tag="div" />
-            <template v-else>{{ description }}</template>
-          </div>
-        </div>
+    <template #body>
+      <div class="page-detail__description has-no-border richtext" tag="main">
+        <p>{{ introText }}</p>
+        <ContentRenderer v-if="body" :value="body" tag="div" />
+        <template v-else>{{ description }}</template>
       </div>
-    </div>
-    <div class="page-detail__sticky-end"></div>
-  </div>
+    </template>
+    <template #intro>
+      <headline
+        :text="computedName"
+        :level="headlineLevel"
+        :classes="
+          'testimonials-detail__headline page-detail__headline' +
+          (headlineClasses ? ` ${headlineClasses}` : ' h2-font-size bold')
+        "
+      />
+      <span class="testimonials-detail__subline page-detail__subline">{{ computedSubline }}</span>
+      <div class="page-detail__img page-detail__animation-3">
+        <v-img v-bind="image" :cloudinary="!cloudinary ? true : image.cloudinary" />
+      </div>
+    </template>
+  </page-detail>
 </template>
 
 <script>
@@ -95,16 +87,6 @@ export default {
         return `${this.title} | ${this.location}`;
       }
       return this.title;
-    },
-    stickyOptions() {
-      return JSON.stringify({
-        parentSelector: '.page-detail__sticky-start',
-        breakpoint: 'lg',
-        startPoint: '.page-detail__sticky-start',
-        endPoint: '.page-detail__sticky-end',
-        stickyOffsetTop: 200,
-        stickyOffsetBottom: 20,
-      });
     },
   },
 };
