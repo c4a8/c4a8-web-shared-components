d3b0adb8e32400e1024203dfe35ee5559e32ca5c
michael.eny.team <michael.ehrich@krekeny.com>
2026-04-14 22:02:46 +0200

    feat(useAsyncData): add unique data key for all async calls

--- dist/runtime/components/shared-content-list.vue
+++ dist/runtime/components/shared-content-list.vue
@@ -6,6 +6,7 @@
 import { useI18n } from 'vue-i18n';
 import { computed } from 'vue';
 import { useAsyncData, queryCollection } from '#imports';
+import Tools from '../utils/tools';
 
 const props = defineProps({
   query: {
@@ -20,25 +21,6 @@ const props = defineProps({
 
 const { locale, strategy } = useI18n();
 
-// A fast, 53-bit hash function (No external lib needed)
-const hash = (str, seed = 0) => {
-  let h1 = 0xdeadbeef ^ seed,
-    h2 = 0x41c6ce57 ^ seed;
-
-  for (let i = 0, ch; i < str.length; i++) {
-    ch = str.charCodeAt(i);
-    h1 = Math.imul(h1 ^ ch, 2654435761);
-    h2 = Math.imul(h2 ^ ch, 1597334677);
-  }
-
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
-  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
-  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-
-  return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString(16);
-};
-
 const localeQuery = computed(() => ({
   ...props.query,
   where: {
@@ -46,9 +28,7 @@ const localeQuery = computed(() => ({
   },
 }));
 
-const whereHash = hash(JSON.stringify(props.query?.where || {}));
-const dataKey =
-  props.query?.key || `content-${locale.value}-${(props.query?.path || 'list').replace(/\//g, '-')}-${whereHash}`;
+const dataKey = props.query?.key || Tools.getDataKey('content', props.query, locale.value);
 
 const filterDuplicateItems = (items) => {
   const seen = new Map();
