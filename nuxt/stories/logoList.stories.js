22b8040944488b4aacb68f09800a9ceab0b224d0
Michael Ehrich <michael.ehrich@krekeny.com>
2026-05-26 23:26:28 +0200

    data(LogoList): add missing story

---
+++ nuxt/stories/logoList.stories.js
@@ -0,0 +1,86 @@
+import LogoListComponent from '../components/logo-list.vue';
+
+export default {
+  component: LogoListComponent,
+  argTypes: {},
+  title: 'Components/LogoList',
+};
+
+export const LogoList = {
+  args: {
+    list: [
+      {
+        img: '/c_lpad,h_60,w_180/logos/cust-dbschenker.png',
+        alt: 'DB Schenker',
+        url: 'https://youtu.be/ABUieErMHLU',
+        title: 'DB Schenker',
+      },
+      {
+        img: '/c_lpad,h_60,w_180/logos/cust-dmgmori.png',
+        alt: 'DMG MORI',
+        url: 'https://de.dmgmori.com',
+        title: 'DMG MORI',
+      },
+      {
+        img: '/c_lpad,h_60,w_180/logos/cust-ewe-netz.png',
+        alt: 'EWE',
+        url: 'https://www.ewe-netz.de',
+        title: 'EWE',
+      },
+      {
+        img: '/c_lpad,h_60,w_180/logos/cust-enbw.png',
+        alt: 'EnBW',
+        url: 'https://www.enbw.com',
+        title: 'EnBW',
+      },
+      {
+        img: '/c_lpad,h_60,w_160/logos/cust-enpal.png',
+        alt: 'Enpal',
+        url: 'https://www.enpal.de',
+        title: 'Enpal',
+      },
+      {
+        img: '/c_lpad,h_120,w_180/logos/cust-fuchs.png',
+        alt: 'Fuchs Schmierstoffe',
+        url: 'https://youtu.be/U2ExmypuUvs',
+        title: 'Fuchs Schmierstoffe',
+      },
+      {
+        img: '/c_lpad,h_60,w_180/logos/cust-freudenberg.png',
+        alt: 'Freudenberg',
+        url: 'https://www.freudenberg.com',
+        title: 'Freudenberg',
+      },
+      {
+        img: '/c_lpad,h_60,w_160/logos/cust-haniel.png',
+        alt: 'Haniel',
+        url: 'https://youtu.be/hV97zDTlDko',
+        title: 'Haniel',
+      },
+      {
+        img: '/c_lpad,h_60,w_180/logos/cust-microsoft.png',
+        alt: 'Microsoft',
+        url: 'https://www.microsoft.com/en-us/microsoft-365/business?rtc=1',
+        title: 'Microsoft',
+      },
+      {
+        img: '/c_lpad,h_60,w_100/logos/cust-swb.png',
+        alt: 'SWB',
+        url: 'https://www.swb.de',
+        title: 'SWB',
+      },
+      {
+        img: '/c_lpad,h_100,w_180/logos/cust-uniper.png',
+        alt: 'Uniper',
+        url: 'https://youtu.be/ThxpyUOz2R8',
+        title: 'Uniper',
+      },
+      {
+        img: '/c_lpad,h_130,w_80/logos/cust-zwilling.png',
+        alt: 'ZWILLING J.A. Henckels Deutschland GmbH',
+        url: 'https://www.zwilling.com/de',
+        title: 'ZWILLING J.A. Henckels Deutschland GmbH',
+      },
+    ],
+  },
+};
