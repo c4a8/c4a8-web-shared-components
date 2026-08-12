f1f0db83dc2a4304e636bd07f5cfd7eba3f334cd
Lisa Nagl <lisa.nagl@glueckkanja.com>
2026-08-12 16:16:33 +0200

    fix(carousel): tile width

--- dist/runtime/components/carousel.vue
+++ dist/runtime/components/carousel.vue
@@ -185,7 +185,8 @@ export default {
   display: flex;
   flex-direction: column;
   width: 100%;
-  max-width: 190px;
+  max-width: 270px;
+  min-width: 250px;
   white-space: normal;
   height: 100%;
   align-content: flex-start;
@@ -198,6 +199,7 @@ export default {
   text-transform: uppercase;
   padding-right: 2.25rem;
   width: 100%;
+  word-wrap: break-word;
 }
 
 .carousel__item-overline {
